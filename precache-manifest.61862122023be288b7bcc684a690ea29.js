bb0c1886e93d7848e211ce9203397e5a88449d76
Sebastian Machaj <team@blackredstudio.com>
2020-06-10 11:24:41 +0200

    Updates

---
+++ precache-manifest.61862122023be288b7bcc684a690ea29.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93aef2cf599eef239781b6cd3ec3eab4",
+    "url": "/react-app-test/index.html"
+  },
+  {
+    "revision": "59ae37f8fd4d0655b43c",
+    "url": "/react-app-test/static/css/main.9791c723.chunk.css"
+  },
+  {
+    "revision": "3f3ef54b6c3ffe8fbd0a",
+    "url": "/react-app-test/static/js/2.859e52d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app-test/static/js/2.859e52d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ae37f8fd4d0655b43c",
+    "url": "/react-app-test/static/js/main.891c70d8.chunk.js"
+  },
+  {
+    "revision": "8eeeb111f6206a3675d7",
+    "url": "/react-app-test/static/js/runtime-main.bdc8c165.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-app-test/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-app-test/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-app-test/static/media/header3.d9d36b65.jpg"
+  }
+]);
